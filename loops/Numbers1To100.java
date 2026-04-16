8cc0fed1ae3d23670d4e8b653a699e626c12bf68
Ritubala Maharana <159517908+ritubalamaharana@users.noreply.github.com>
2026-04-16 22:13:04 +0530

    Add files via upload

---
+++ loops/Numbers1To100.java
@@ -0,0 +1,8 @@
+public class Numbers1To100 {
+    static void main(String[] args) {
+        System.out.println("The numbers from 1 to 100 is:");
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
