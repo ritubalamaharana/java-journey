93400aa99f569b135ff624fc27aff1458b4cfc0a
Ritubala Maharana <159517908+ritubalamaharana@users.noreply.github.com>
2026-04-21 20:16:34 +0530

    Add files via upload

---
+++ loops/StarPattern.java
@@ -0,0 +1,9 @@
+public class StarPattern {
+    static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
